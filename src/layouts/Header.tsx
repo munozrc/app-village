bba44e11fa596213935635a4be33d17c7b2e4b3c
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-12 15:32:40 -0500

    styles button change theme

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer } from "./ParentContainer";
 import styled from "styled-components";
+import { VscSearch } from "react-icons/vsc";
 
 type HeaderProps = {
   toggleTheme: (event: React.MouseEvent<HTMLButtonElement>) => void;
@@ -14,7 +15,16 @@ export default function Header({ toggleTheme }: HeaderProps) {
         </RectangleLogo>
       </ContainerLogo>
       <ContainerTools>
-        <button onClick={toggleTheme}>Theme</button>
+        <WrapperInput>
+          <SearchInput
+            type={"text"}
+            placeholder={"Buscar programas o archivos..."}
+          />
+          <SearchButton>
+            <VscSearch size={"1.2em"} />
+          </SearchButton>
+        </WrapperInput>
+        <ThemeButton onClick={toggleTheme}>Tema</ThemeButton>
       </ContainerTools>
     </HeaderContainer>
   );
@@ -31,8 +41,9 @@ const ContainerLogo = styled.div`
 const ContainerTools = styled.div`
   grid-area: tools;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
+  padding: 0px 20px;
   border-right: 1px solid rgba(68, 68, 68, 0.2);
 `;
 
@@ -47,4 +58,94 @@ const Logo = styled.h1`
   font-weight: 800;
   letter-spacing: 2px;
   color: ${({ theme }) => theme.background};
+`;
+
+const WrapperInput = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 40px;
+`;
+
+const SearchInput = styled.input`
+  min-width: 300px;
+  height: 100%;
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 4px;
+  padding-left: 10px;
+  margin-right: 5px;
+  background: ${({ theme }) => theme.background};
+  font-size: 1em;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 0px 2px #666;
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.text};
+  }
+`;
+
+const SearchButton = styled.button`
+  width: 40px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 0px 2px #666;
+  }
+
+  & svg {
+    fill: ${({ theme }) => theme.background};
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.background};
+  }
+
+  &:hover svg {
+    fill: ${({ theme }) => theme.text};
+  }
+`;
+
+const ThemeButton = styled.button`
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${({ theme }) => theme.text};
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 4px;
+  padding: 0px 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  margin-left: 5px;
+  color: ${({ theme }) => theme.background};
+  font-size: 1em;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 0px 2px #666;
+  }
+
+  & svg {
+    fill: ${({ theme }) => theme.background};
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
+  }
+
+  &:hover svg {
+    fill: ${({ theme }) => theme.text};
+  }
 `;
