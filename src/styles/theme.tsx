398ae978ed6e10165f2a302491e95cca7af13791
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-12 19:08:35 -0500

    add new properties

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -4,15 +4,18 @@ declare module "styled-components" {
   export interface DefaultTheme {
     text: string;
     background: string;
+    outlineColor: string;
   }
 }
 
 export const lightTheme: DefaultTheme = {
   text: "#181818",
   background: "#fff",
+  outlineColor: "#d72323",
 };
 
 export const darkTheme: DefaultTheme = {
   text: "#fff",
   background: "#181818",
+  outlineColor: "#ff5959",
 };
