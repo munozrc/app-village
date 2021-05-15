be790e42a3bcfd4c01f7537c55c12af372ac70f8
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-15 13:18:21 -0500

    add setting config theme in localstorage

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,11 +1,21 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function useTheme() {
   const [theme, setTheme] = useState("light");
 
+  const setMode = useCallback((mode: string) => {
+    window.localStorage.setItem("theme", mode);
+    setTheme(mode);
+  }, []);
+
   const toggleTheme = useCallback(() => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  }, [theme]);
+    theme === "light" ? setMode("dark") : setMode("light");
+  }, [setMode, theme]);
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
 
   return {
     theme,
