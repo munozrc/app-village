492962e13f6601b464ff750b9b3972fff3eb44ee
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-01 17:02:38 -0500

    print consolo load files info.md

--- src/scripts/create-programs-data.ts
+++ src/scripts/create-programs-data.ts
@@ -8,15 +8,31 @@ let programsData: program[];
 function main() {
   fs.readdir(programsFolder, (err, list) => {
     if (err) return console.log(err);
+
+    // Read files info.md for each program
+    console.info("[+] Reading files info.md");
     list.forEach((path) => getInfoProgram(path));
   });
 }
 
 function getInfoProgram(path: string) {
-  fs.readFile(`${programsFolder}/${path}/info.md`, "utf8", (err, _content) => {
-    if (err) return console.log("[-] Failed to read");
-    console.log("[+] Read file");
+  fs.readFile(`${programsFolder}/${path}/info.md`, "utf8", (err, content) => {
+    if (err) return console.log(err);
+
+    const lines = content.split("\n");
+    const metadataIndices: number[] = lines.reduce(getMetadataIndice, []);
+
+    if (metadataIndices.length === 0)
+      console.log(`    [-] No found file info.md in ${path}`);
+    else console.log(`    [+] ${path} is OK!`);
   });
+}
+
+function getMetadataIndice(acc: number[], elem: string, index: number) {
+  if (/^---/.test(elem)) {
+    acc.push(index);
+  }
+  return acc;
 }
 
 // init script
