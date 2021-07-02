c89cf62d65a9ce98d970bc31e2044298f6017fe3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-01 23:39:33 -0500

    resolve promise readfiles

--- src/scripts/create-programs-data.ts
+++ src/scripts/create-programs-data.ts
@@ -1,50 +1,10 @@
 import * as path from "path";
-import * as fs from "fs";
+import { promises as fs } from "fs";
 import { program } from "../types";
 
 let programsFolder = path.join(process.cwd(), "/content/programs");
 let programsOutput = path.join(process.cwd(), "src/data/programs.json");
 let programsData: program[] = [];
-
-function main() {
-  fs.readdir(programsFolder, (err, list) => {
-    if (err) return console.log(err);
-
-    // Read files info.md for each program
-    console.info("[+] Reading files info.md");
-    list.forEach((path) => getInfoProgram(path));
-  });
-
-  // Temporal print
-  console.log({ programsData });
-}
-
-function getInfoProgram(path: string) {
-  fs.readFile(`${programsFolder}/${path}/info.md`, "utf8", (err, content) => {
-    if (err) return console.log(err);
-
-    const lines = content.split("\n");
-    const metadataIndices: number[] = lines.reduce(getMetadataIndice, []);
-
-    if (metadataIndices.length === 0)
-      return console.log(`    [-] No found file info.md in ${path}`);
-
-    // get values metadata
-    const metadata = parseMetadata(lines, metadataIndices);
-    const date = new Date(metadata.date);
-    const timestamp = date.getTime() / 1000;
-
-    programsData.push({
-      id: timestamp,
-      name: metadata.name,
-      path: metadata.path,
-      date: metadata.date,
-    });
-
-    // print message
-    console.log(`    [+] ${path} is OK!`);
-  });
-}
 
 function getMetadataIndice(acc: number[], elem: string, index: number) {
   if (/^---/.test(elem)) {
@@ -64,5 +24,42 @@ function parseMetadata(lines: string[], metadataIndices: number[]) {
   return metadata;
 }
 
-// init script
-main();
+function getInfoProgram(data: string, path: string) {
+  const lines = data.split("\n");
+  const metadataIndices: number[] = lines.reduce(getMetadataIndice, []);
+
+  if (metadataIndices.length === 0)
+    return console.log(`    [-] No found file info.md in ${path}`);
+
+  // get values metadata
+  const metadata = parseMetadata(lines, metadataIndices);
+  const date = new Date(metadata.date);
+  const timestamp = date.getTime() / 1000;
+
+  programsData.push({
+    id: timestamp,
+    name: metadata.name,
+    path: metadata.path,
+    date: metadata.date,
+  });
+
+  // print message
+  console.log(`    [+] ${path} is OK!`);
+}
+
+// Main Function Script
+(async () => {
+  const dirs = await fs.readdir(programsFolder);
+
+  // Read files info.md for each program
+  console.info("[+] Reading files info.md");
+  Promise.all(
+    dirs.map((path) => {
+      return fs.readFile(`${programsFolder}/${path}/info.md`, "utf8");
+    })
+  )
+    .then((fileBuffers) => {
+      fileBuffers.forEach((data, index) => getInfoProgram(data, dirs[index]));
+    })
+    .then(() => console.log({ programsData }));
+})();
