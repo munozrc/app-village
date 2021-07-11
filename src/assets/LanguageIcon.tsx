8fd5fe94ef7579a1bce80365708f475428ddb16e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-10 22:42:48 -0500

    add new icons

---
+++ src/assets/LanguageIcon.tsx
@@ -0,0 +1,19 @@
+function LanguageIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+    >
+      <circle cx={12} cy={12} r={10} />
+      <path d="M2 12h20M12 2a15.3 15.3 0 014 10 15.3 15.3 0 01-4 10 15.3 15.3 0 01-4-10 15.3 15.3 0 014-10z" />
+    </svg>
+  );
+}
+
+export default LanguageIcon;
