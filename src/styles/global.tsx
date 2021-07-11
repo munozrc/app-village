ebdabc1c93e7a6b0efaf03729b481a41a8972557
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-11 16:30:56 -0500

    change font

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -7,7 +7,7 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: 'Open Sans', sans-serif;
     color: #fff;
   }
 
