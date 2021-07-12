c333d7491a45e54492d3e3ff62d032376648b8a6
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-07-12 17:50:25 -0500

    new icons for More Programs Link

---
+++ src/assets/GridIcon.tsx
@@ -0,0 +1,18 @@
+function GridIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+    >
+      <path d="M3 3h7v7H3zM14 3h7v7h-7zM14 14h7v7h-7zM3 14h7v7H3z" />
+    </svg>
+  );
+}
+
+export default GridIcon;
