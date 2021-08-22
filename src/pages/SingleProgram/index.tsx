e13844afb6bdf704feedcd5f49269016eafa85d8
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-08-21 23:24:00 -0500

    create handleDownload and handleChangeversion

--- src/pages/SingleProgram/index.tsx
+++ src/pages/SingleProgram/index.tsx
@@ -11,6 +11,7 @@ import LanguageIcon from "../../assets/LanguageIcon";
 import LinkIcon from "../../assets/LinkIcon";
 import DownloadIcon from "../../assets/DownloadIcon";
 import ReactMarkdown from "react-markdown";
+import { useState } from "react";
 
 interface SingleProgramParams {
   id: string;
@@ -18,7 +19,25 @@ interface SingleProgramParams {
 
 function SingleProgram() {
   const { id } = useParams<SingleProgramParams>();
+  const [currentVersion, setCurrentVersion] = useState<number>(0);
   const currentProgram = getCurrentProgram(parseInt(id));
+
+  const handleDownload = () => {
+    if (!currentProgram || !currentProgram.versions) return;
+    window.open(currentProgram.versions[currentVersion].installer, "_blank");
+  };
+
+  const handleChangeVersion = () => {
+    if (!currentProgram || !currentProgram.versions) return;
+
+    const maxVersionSupport = currentProgram.versions.length - 1;
+
+    if (maxVersionSupport < currentVersion) {
+      setCurrentVersion((prev) => prev + 1);
+    } else if (maxVersionSupport === currentVersion) {
+      setCurrentVersion(() => 0);
+    }
+  };
 
   if (!currentProgram || !currentProgram.versions) return <h4>404</h4>;
   return (
@@ -33,20 +52,20 @@ function SingleProgram() {
           <h2 className="single-program__name">{currentProgram.name}</h2>
         </header>
         <div className="single-program__items-container">
-          <span className="single-program__item">
+          <span className="single-program__item" onClick={handleDownload}>
             <DownloadIcon /> Descargar
           </span>
-          <span className="single-program__item">
-            <PackageIcon /> {currentProgram.versions[0].name}
+          <span className="single-program__item" onClick={handleChangeVersion}>
+            <PackageIcon /> {currentProgram.versions[currentVersion].name}
           </span>
           <span className="single-program__item">
-            <SystemIcon /> {currentProgram.versions[0].os}
+            <SystemIcon /> {currentProgram.versions[currentVersion].os}
           </span>
           <span className="single-program__item">
-            <StorageIcon /> {currentProgram.versions[0].size}
+            <StorageIcon /> {currentProgram.versions[currentVersion].size}
           </span>
           <span className="single-program__item">
-            <LanguageIcon /> {currentProgram.versions[0].language}
+            <LanguageIcon /> {currentProgram.versions[currentVersion].language}
           </span>
           <span className="single-program__item">
             <LinkIcon /> {currentProgram.dev}
@@ -54,7 +73,9 @@ function SingleProgram() {
         </div>
       </section>
       <section className="single-program__content">
-        <ReactMarkdown children={currentProgram.versions[0].content} />
+        <ReactMarkdown
+          children={currentProgram.versions[currentVersion].content}
+        />
       </section>
     </div>
   );
