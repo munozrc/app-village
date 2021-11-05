0528332f8dbb2659838a134b04a5a28f64942c75
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-11-05 07:48:18 -0500

    migrate project to vite 😬

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/softlink',
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
+  },
+  plugins: [reactRefresh()]
+})
