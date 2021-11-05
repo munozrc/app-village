b3c423ddf8c3298557d90016f78eba1747e4ed8f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-11-05 10:05:19 -0500

    refactor(Root): Add React import and add css modules

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
-import "./styles.css";
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css'
 
-function Header() {
+const Header: FC<{}> = () => {
   return (
-    <header className="header">
-      <Link to="/" className="header__logo">
+    <header className={styles.wrapper}>
+      <Link to='/' className={styles.logo}>
         <h1>SOFTLINK</h1>
       </Link>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
