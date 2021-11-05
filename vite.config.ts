88bcb660986628e781b79b4071b4bfbbf27b1b98
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-11-05 09:55:20 -0500

    fix(Root): Add slash base option vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/softlink',
+  base: '/softlink/',
   esbuild: {
     jsxFactory: '_jsx',
     jsxFragment: '_jsxFragment',
