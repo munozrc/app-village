a718de9df06ba8f4cf5cb81298306d75e6ec25b4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-16 14:49:36 -0500

    feat: use new app entity

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import App from './App'
+import { PageLayout } from './app/components/PageLayout'
+import { HomePage } from './app/pages/HomePage'
+import { ProgramsPage } from './program/pages'
+
 import './main.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <PageLayout>
+    <Routes>
+      <Route path="/programs/:id" element={<div>working progress</div>} />
+      <Route path="/programs" element={<ProgramsPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    </PageLayout>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
