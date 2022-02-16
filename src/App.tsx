76a68aa105fdb3867599cdd0df8981b61cdc50a0
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 20:04:59 -0500

    refactor: import/orden to App and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import './App.css'
 import Header from './components/Header'
 import AllPrograms from './pages/AllPrograms'
 import Home from './pages/Home'
 import SingleProgram from './pages/SingleProgram'
+
+import './App.css'
 
 const App: FC<{}> = () => {
   return (
