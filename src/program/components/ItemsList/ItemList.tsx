c23eb929bb9588544d0174b99d4a4bd990f9057b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 21:31:36 -0500

    feat: Add new component for show itemList

---
+++ src/program/components/ItemsList/ItemList.tsx
@@ -0,0 +1,27 @@
+import { Item } from './Item'
+import styles from './ItemList.module.css'
+
+interface ItemElement {
+  id: string,
+  icon: string,
+  name: string
+}
+
+interface ItemListProps {
+  items: ItemElement[]
+}
+
+export const ItemList = ({ items } : ItemListProps) => {
+  return (
+    <div className={styles.itemListContainer}>
+      {items.map(({ id, icon, name }) => (
+        <Item
+          key={id}
+          href={`/programs/${id}`}
+          icon={icon}
+          name={name}
+        />
+      ))}
+    </div>
+  )
+}
