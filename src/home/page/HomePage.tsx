cd0d9603d94767e9c03f0f3366d44729a4084ca3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 23:58:21 -0500

    feat: Create HomePage component

---
+++ src/home/page/HomePage.tsx
@@ -0,0 +1,11 @@
+import { ProgramSection } from '../../program/components'
+
+import styles from './HomePage.module.css'
+
+export const HomePage = () => {
+  return (
+    <div className={styles.homePage}>
+      <ProgramSection />
+    </div>
+  )
+}
