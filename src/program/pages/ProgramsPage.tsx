d56a0f1323cbbe37ad7ad6b9dc90a44dc308d426
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 21:59:55 -0500

    feat: Add new styles and use custom hook

--- src/program/pages/ProgramsPage.tsx
+++ src/program/pages/ProgramsPage.tsx
@@ -1,7 +1,21 @@
+import { SearchIcon } from '../../components/Icons'
+import { ItemList } from '../components/ItemsList'
+import { usePrograms } from '../hooks/usePrograms'
+
 import styles from './ProgramsPage.module.css'
 
 export const ProgramsPage = () => {
-  return <div className={styles.programsPage}>
-    <h1>Programs Page</h1>
-  </div>
+  const { normalizeListItems } = usePrograms()
+
+  return (
+    <div className={styles.programsPage}>
+      <form className={styles.bar} onSubmit={() => {}}>
+        <input className={styles.inputSearch} placeholder="Buscar..." />
+        <button className={styles.btnSearch}>
+          <SearchIcon />
+        </button>
+      </form>
+      <ItemList items={normalizeListItems()} />
+    </div>
+  )
 }
