f4beb9e880333802dd80ca1e200075207e991724
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 23:58:42 -0500

    feat: Use new component HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header'
-import Home from './pages/Home'
 import SingleProgram from './pages/SingleProgram'
 import { ProgramsPage } from './program/pages'
+import { HomePage } from './home/page'
 
-import './App.css'
-
-const App: FC<{}> = () => {
+const App = () => {
   return (
-    <main className="parent-container">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/programs/:id" element={<SingleProgram />} />
-          <Route path="/programs" element={<ProgramsPage />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/programs/:id" element={<SingleProgram />} />
+        <Route path="/programs" element={<ProgramsPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
