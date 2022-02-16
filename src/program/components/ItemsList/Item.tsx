21c898685fa955bcfd763f999e55458092861bff
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-15 21:31:16 -0500

    feat: Add new Item for list Programs

---
+++ src/program/components/ItemsList/Item.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+
+import { ArrowIcon } from '../../../components/Icons'
+
+import styles from './Item.module.css'
+
+interface ItemProps {
+  href: string
+  icon: string
+  name: string
+}
+
+export const Item = ({ href, icon, name }: ItemProps) => {
+  return (
+    <Link to={href} className={styles.itemContainer}>
+      <img className={styles.icon} src={icon} alt={name} />
+      <h3 className={styles.title}>{name}</h3>
+      <span className={styles.moreInfo}>
+        <ArrowIcon />
+          Más Información
+        <ArrowIcon />
+      </span>
+    </Link>
+  )
+}
