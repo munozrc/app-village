3caf5afa1cf484d1ce494f88a3e30657d00aaacf
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-16 23:09:12 -0500

    feat: use new page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { PageLayout } from './app/components/PageLayout'
 import { HomePage } from './app/pages/HomePage'
+import { ProgramPage } from './app/pages/ProgramPage'
 import { ProgramsPage } from './app/pages/ProgramsPage'
 
 import './main.css'
@@ -11,7 +12,7 @@ ReactDOM.render(
   <BrowserRouter>
     <PageLayout>
     <Routes>
-      <Route path="/programs/:id" element={<div>working progress</div>} />
+      <Route path="/programs/:id" element={<ProgramPage />} />
       <Route path="/programs" element={<ProgramsPage />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
