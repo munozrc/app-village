7a7822728912e04d9b9ad0c68501ceb66ae5d7ae
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-09 23:55:00 -0500

    feat: Set styles css and format to PostPage

--- src/app/pages/PostPage.tsx
+++ src/app/pages/PostPage.tsx
@@ -1,9 +1,33 @@
+import { useParams } from 'react-router'
+import ReactMarkdown from 'react-markdown'
+
+import { LinkRenderer } from '../components/LinkRenderer'
+import listPosts from '../../post/data.json'
+
 import styles from './PostPage.module.css'
 
+const getDateTime = (date: string) => new window.Intl.DateTimeFormat('es', { dateStyle: 'full' }).format(new Date(date))
+
 export const PostPage = () => {
+  const { path } = useParams()
+  const post = listPosts.find(post => post.path === path)
+
+  if (typeof post === 'undefined') return null
+
   return (
     <div className={styles.container}>
-      <h1>Post</h1>
+      <header>
+        <p className={styles.post__date}>
+          {getDateTime(post.date)}
+          <span className={styles.post__dot}></span>
+          Post
+        </p>
+        <h2 className={styles.post__title}>{post.title}</h2>
+        <img className={styles.post__image} src={post.heroImage} alt={post.title} />
+      </header>
+      <section className={styles.post__content}>
+        <ReactMarkdown components={{ a: LinkRenderer }}>{post.content}</ReactMarkdown>
+      </section>
     </div>
   )
 }
