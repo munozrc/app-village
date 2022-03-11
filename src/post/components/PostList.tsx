05706d6b5059bc0d6ab6c771edeea808d0f236f2
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-11 18:32:23 -0500

    feat: Add thumbnail image for post into homepage

--- src/post/components/PostList.tsx
+++ src/post/components/PostList.tsx
@@ -11,6 +11,7 @@ export const PostList = () => {
         listPosts.slice(0, 4).map(post => (
           <li key={post.id} className={styles.post__wrapper}>
             <Link to={`/posts/${post.path}`} className={styles.post__content}>
+              <img className={styles.post__thumbnail} src={post.heroImage} alt={post.title}/>
               <h4 className={styles.post__title}>{post.title}</h4>
               <p className={styles.post__description}>{post.description}</p>
               <span className={styles.post__button} role="button">Leer &#8594;</span>
