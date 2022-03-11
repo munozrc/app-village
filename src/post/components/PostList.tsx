6622e15c4762f90eb5a1f5713cce0c44b3a88796
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-11 18:14:34 -0500

    feat: Use new parameter description into post

--- src/post/components/PostList.tsx
+++ src/post/components/PostList.tsx
@@ -12,7 +12,7 @@ export const PostList = () => {
           <li key={post.id} className={styles.post__wrapper}>
             <Link to={`/posts/${post.path}`} className={styles.post__content}>
               <h4 className={styles.post__title}>{post.title}</h4>
-              <p className={styles.post__description}>Puedes encontrar algunos sitios y tips para realizar una buena consulta de fuentes como articulos o publicaciones.</p>
+              <p className={styles.post__description}>{post.description}</p>
               <span className={styles.post__button} role="button">Leer &#8594;</span>
             </Link>
           </li>
