a360e73f4c19548c0aeaec3f052bd0738f75a4c7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-04-09 16:00:57 -0500

    feat: Add icons components

---
+++ components/Icons/LanguageIcon.jsx
@@ -0,0 +1,18 @@
+export const LanguageIcon = (props) => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <circle cx={12} cy={12} r={10} />
+      <path d="M2 12h20M12 2a15.3 15.3 0 014 10 15.3 15.3 0 01-4 10 15.3 15.3 0 01-4-10 15.3 15.3 0 014-10z" />
+    </svg>
+  )
+}
