40c274a95ce96428fa00dc4c5daf4b2975279ddb
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-04-09 17:38:58 -0500

    feat: Add new file with authors

---
+++ posts/authors.js
@@ -0,0 +1,10 @@
+const authors = [
+  {
+    displayName: "Carlos Muñoz",
+    nickname: "munozrc",
+    photo: "https://avatars.githubusercontent.com/u/47870821?v=4",
+    website: "https://github.com/munozrc"
+  }
+]
+
+export default authors
