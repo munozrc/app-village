caec18a8cb1b588882fb0d116e5c37261f96cdb2
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-08-05 16:11:59 -0500

    Update home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,36 +1,18 @@
-import { Link } from 'react-router-dom'
 import SearchField from '../components/search-field'
 
 import styles from '../styles/home.module.css'
 
 export const Home = () => {
   return (
-    <main className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.wrapper}>
-          <section className={styles.navbar}>
-            <h1 className={styles.branding}>
-              <Link to="/">AppVillage</Link>
-            </h1>
-            <nav className={styles.navigation}>
-              <Link to="/desktop">Desktop</Link>
-              <Link to="/android">Android</Link>
-              <Link to="/drivers">Drivers</Link>
-            </nav>
-          </section>
-          <section className={styles.wrapperHero}>
-            <div className={styles.hero}>
-              <h2>La fuente de Internet de Software gratuito.</h2>
-              <p>Recopilación de enlaces directos a programas, drivers y aplicaciones móviles.</p>
-            </div>
-            <div className={styles.center}>
-              <SearchField onSubmit={() => {}}/>
-            </div>
-          </section>
+    <main className={styles.wrapper}>
+      <section className={styles.container}>
+        <div className={styles.hero}>
+          <h2>Un pequeño lugar en Internet de Software gratuitos.</h2>
+          <p>Recopilación de enlaces directos a programas, drivers y aplicaciones móviles.</p>
         </div>
-      </header>
-      <section className={styles.content}>
-        <div className={styles.wrapper}></div>
+        <div className={styles.search}>
+          <SearchField onSubmit={() => {}}/>
+        </div>
       </section>
     </main>
   )
