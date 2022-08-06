ad7b4ea2f9d005ef1cbe12468030124989fd8e3f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-08-06 15:21:23 -0500

    Add styles and add useParams for get category

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -1,13 +1,17 @@
+import { useParams } from 'react-router-dom'
+import { SearchField } from '../components'
 import styles from '../styles/apps.module.css'
 
 export const Apps = () => {
+  const { category } = useParams()
   return (
-    <main>
+    <main className={styles.wrapper}>
       <section className={styles.container}>
         <header className={styles.title}>
-          <h2>Todas las aplicaciones</h2>
-          <p>Busqueda por categoria Desktop</p>
+          <h2>Todas las Aplicaciones</h2>
+          <p>Busqueda por categoria <strong>{category}</strong></p>
         </header>
+        <SearchField onSubmit={() => {}}/>
       </section>
     </main>
   )
