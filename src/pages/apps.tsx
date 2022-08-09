3cd0d1eb59ee4a2d65e0b7649257efbbd3f645f8
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-08-09 17:59:18 -0500

    Add styles to list apps

--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { SearchField } from '../components'
 
 import data from '../data.json'
+import { DownloadIcon, PackageIcon } from '../icons'
 
 import styles from '../styles/apps.module.css'
 
@@ -16,11 +17,29 @@ export const Apps = () => {
         </header>
         <SearchField onSubmit={() => {}}/>
       </section>
-      <ul>
-        {data.desktop.map((app) => (
-          <li key={app.name}><img src={app.icon}/></li>
-        ))}
-      </ul>
+      <section className={styles.container}>
+        <ul className={styles.results}>
+          {data.desktop.map((app) => (
+            <li key={app.icon}>
+              <header>
+                <img src={app.icon}/>
+                <h4>{app.name}</h4>
+              </header>
+              <p>{app.description}</p>
+              <footer>
+                <button>
+                  <PackageIcon size={24}/>
+                  {app.versions[0].name}
+                </button>
+                <button>
+                  <DownloadIcon size={24}/>
+                  {`Descargar (${app.versions[0].extension})`}
+                </button>
+              </footer>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   )
 }
