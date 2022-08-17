2fb35484dcf1a3a6ba14475dacbb2a6d114c17ab
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-08-17 17:21:17 -0500

    Add github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { basename } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = true
+const folderName = basename(process.cwd()) + '/'
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const base = mode === 'production' && isGitHubPages ? '/' + folderName : '/'
+
 export default defineConfig({
+  base,
   root: "src",
   plugins: [react()],
   publicDir: "../public"
